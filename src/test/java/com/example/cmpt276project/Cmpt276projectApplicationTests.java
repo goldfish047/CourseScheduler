6aefa1652f81b58ae035aaf1b6cbe28dd93162fa
Jagjot Sidhu <jagjotsidhu@Jagjots-MacBook-Air.local>
2023-06-29 23:17:11 -0700

    init push

---
+++ src/test/java/com/example/cmpt276project/Cmpt276projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmpt276project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmpt276projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
