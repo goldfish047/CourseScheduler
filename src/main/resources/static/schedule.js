9de4d0726f23236caee0c7d51d7801b75430672c
Raghad <selling@RaghadsMac.local>
2023-08-01 00:16:47 -0700

    edit schedule table

--- src/main/resources/static/schedule.js
+++ src/main/resources/static/schedule.js
@@ -1,11 +1,9 @@
-
 fetch('Output4.json')
     .then(response => response.json())
     .then(data => {
         const timetable = document.getElementById('schedule');
         const timetableBody = document.getElementById('schedule-body');
 
-    // Find all unique room numbers and sort them
         const roomNumbers = Array.from(new Set(data.map(entry => entry["Room Number"])))
         .sort((a, b) => a - b);
 
@@ -41,8 +39,10 @@ fetch('Output4.json')
         const populateTimetable = (data) => {
             timetableBody.innerHTML = '';
       
-            const topRow = document.createElement('tr');
-            topRow.appendChild(document.createElement('td')); 
+            const topRow = document.createElement('tr'); 
+            const titleCell = document.createElement('th');
+            titleCell.textContent = "Schedule"; 
+            topRow.appendChild(titleCell);
 
             roomNumbers.forEach(roomNumber => {
                 const th = document.createElement('th');
@@ -58,6 +58,7 @@ fetch('Output4.json')
             
                 const timeCell = document.createElement('td');
                 timeCell.textContent = time;
+                timeCell.classList.add('time-column');
                 row.appendChild(timeCell);
 
                 roomNumbers.forEach(roomNumber => {
@@ -65,8 +66,12 @@ fetch('Output4.json')
                     const entry = data.find(entry => entry["Room Number"] === roomNumber && entry["Time"] === time);
 
                     if (entry) {
-                        td.innerHTML = entry["Course"] + "<br>" +  entry["Instructor Num"] + "<br>" + "Section " + entry["Section"];
-                    }
+                        td.innerHTML = `<span class="course">${entry["Course"]}</span><br>` +
+                           `<span class="instructor">${entry["Instructor Num"]}</span><br>` +
+                           `<span class="section">Section ${entry["Section"]}</span>`;
+                    }else {
+                        td.style.background = 'repeating-linear-gradient(45deg, #bebdbd, #bebdbd 10px, #9c9c9c 10px, #a2a1a1 20px)';
+                      }
 
                     row.appendChild(td);
                 });
