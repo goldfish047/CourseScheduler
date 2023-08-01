72a973fb24b290393e6cf862503b151458198e84
Raghad <selling@RaghadsMac.local>
2023-07-31 23:03:47 -0700

    reformat schedule table and edit home page

--- src/main/resources/static/schedule.js
+++ src/main/resources/static/schedule.js
@@ -3,48 +3,78 @@ fetch('Output4.json')
     .then(response => response.json())
     .then(data => {
         const timetable = document.getElementById('schedule');
-        const headerRow = document.getElementById('header-row');
         const timetableBody = document.getElementById('schedule-body');
+
+    // Find all unique room numbers and sort them
+        const roomNumbers = Array.from(new Set(data.map(entry => entry["Room Number"])))
+        .sort((a, b) => a - b);
 
         const columnOrder = [
             "Course",
             "Section",
             "Time", 
-            "Subject to Enrolment",
-            "Program",
             "Instructor Num",
             "Room Number"
         ];
 
-        //Generate header row   
-        columnOrder.forEach(column => {
-            const th = document.createElement('th');
-            th.textContent = column; 
-            headerRow.appendChild(th);
-        });
+        const dayMap = {
+            "Monday AM": 0,
+            "Monday PM": 1,
+            "Monday EVE": 2,
+            "Tuesday AM": 3,
+            "Tuesday PM": 4,
+            "Tuesday EVE": 5,
+            "Wednesday AM": 6,
+            "Wednesday PM": 7,
+            "Wednesday EVE": 8,
+            "Thursday AM": 9,
+            "Thursday PM": 10,
+            "Thursday EVE": 11,
+            "Friday AM": 12,
+            "Friday PM": 13,
+            "Friday EVE": 14,
+          };
 
-        // populate timetable body 
+        const sortedTimes = Array.from(new Set(data.map(entry => entry["Time"])))
+          .sort((a, b) => dayMap[a] - dayMap[b]);
+
         const populateTimetable = (data) => {
             timetableBody.innerHTML = '';
+      
+            const topRow = document.createElement('tr');
+            topRow.appendChild(document.createElement('td')); 
 
-            data.forEach(entry => {
+            roomNumbers.forEach(roomNumber => {
+                const th = document.createElement('th');
+                th.textContent = "Room " + roomNumber;
+                topRow.appendChild(th);
+            });
+
+            timetableBody.appendChild(topRow);
+
+            sortedTimes.forEach(time => {
                 const row = document.createElement('tr');
 
-                
-                columnOrder.forEach(columnSlot => {
-                    const td = document.createElement('td');
-                    td.textContent = entry[columnSlot]
-                    row.appendChild(td);
-                })
+            
+                const timeCell = document.createElement('td');
+                timeCell.textContent = time;
+                row.appendChild(timeCell);
 
+                roomNumbers.forEach(roomNumber => {
+                    const td = document.createElement('td');
+                    const entry = data.find(entry => entry["Room Number"] === roomNumber && entry["Time"] === time);
+
+                    if (entry) {
+                        td.innerHTML = entry["Course"] + "<br>" +  entry["Instructor Num"] + "<br>" + "Section " + entry["Section"];
+                    }
+
+                    row.appendChild(td);
+                });
 
                 timetableBody.appendChild(row);
-            })
-
-
+            });
         };
-
-
+        
         populateTimetable(data);
     })
     .catch(error => console.error(error));
